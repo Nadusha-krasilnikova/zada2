3927467b625f70afc9325cde3f04c2417d2b4c3b
Nadusha <nadusha230275@gmail.com>
2022-07-02 14:06:00 +0500

    находит только максимальное

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// даны два числа. Показать большее и меньшее число
+int a=10;
+int b=15;
+int max=a;
+if (a>max) max=a;
+if (b>max) max=b;
+
+Console.Write("максимальное число ");
+Console.WriteLine(max);
+
